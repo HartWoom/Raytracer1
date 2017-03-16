26006f5c91a5b8243da0a9c547702e14d5200671
HartWoom <antoine.hartwig@epitech.eu>
2017-03-16 14:25:52 +0100

    Enieme correction du rotate

--- src/rotate.c
+++ src/rotate.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.hartwig@epitech.eu>
 ** 
 ** Started on  Tue Mar  7 16:59:50 2017 HartWoom
-** Last update Thu Mar 16 13:22:14 2017 HartWoom
+** Last update Thu Mar 16 14:25:20 2017 HartWoom
 */
 
 #include "../include/my.h"
@@ -15,8 +15,10 @@ sfVector3f	rotate_arround_x(sfVector3f vector, float angle)
   sfVector3f	to_return;
 
   to_return = vector;
-  to_return.y = vector.y * cos(angle) + vector.z * -sin(angle);
-  to_return.z = vector.y * sin(angle) + vector.z * cos(angle);
+  to_return.y = vector.y * (M_PI * cosf(angle) / 180)
+    + vector.z * (M_PI * -sin(angle) / 180);
+  to_return.z = vector.y * (M_PI * sinf(angle) / 180)
+    + vector.z * (M_PI * cos(angle) / 180);
   return (to_return);
 }
 
@@ -25,8 +27,10 @@ sfVector3f	rotate_arround_y(sfVector3f vector, float angle)
   sfVector3f	to_return;
 
   to_return = vector;
-  to_return.x = vector.x * cos(angle) + vector.z * sin(angle);
-  to_return.z = vector.x * -sin(angle) + vector.z * cos(angle);
+  to_return.x = vector.x * (M_PI * cos(angle) / 180)
+    + vector.z * (M_PI * sin(angle) / 180);
+  to_return.z = vector.x * (M_PI * -sin(angle) / 180)
+    + vector.z * (M_PI * cos(angle) / 180);
   return (to_return);
 }
 
@@ -35,13 +39,16 @@ sfVector3f	rotate_arround_z(sfVector3f vector, float angle)
   sfVector3f	to_return;
 
   to_return = vector;
-  to_return.x = vector.x * cos(angle) + vector.y * -sin(angle);
-  to_return.y = vector.x * sin(angle) + vector.y * cos(angle);
+  to_return.x = vector.x * (M_PI * cos(angle) / 180)
+    + vector.y * (M_PI * -sin(angle) / 180);
+  to_return.y = vector.x * (M_PI * sin(angle) / 180)
+    + vector.y * (M_PI * cos(angle) / 180);
   return (to_return);
 }
 
 sfVector3f	rotate_xyz(sfVector3f to_rotate, sfVector3f angles)
 {
+  
   to_rotate = rotate_arround_x(to_rotate, angles.x);
   to_rotate = rotate_arround_y(to_rotate, angles.y);
   to_rotate = rotate_arround_z(to_rotate, angles.z);
