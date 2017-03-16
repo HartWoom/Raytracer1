e1cf7c1a5551111e731fd47c38cf7c9c33717332
HartWoom <antoine.hartwig@epitech.eu>
2017-03-16 10:15:40 +0100

    Rotation finie et gestion de division par 0 dans light.c

--- src/light.c
+++ src/light.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.hartwig@epitech.eu>
 ** 
 ** Started on  Tue Mar  7 17:33:58 2017 HartWoom
-** Last update Thu Mar 16 09:48:31 2017 HartWoom
+** Last update Thu Mar 16 10:08:47 2017 HartWoom
 */
 
 #include "../include/my.h"
@@ -15,11 +15,13 @@ float	get_light_coef(sfVector3f light, sfVector3f normal)
   float	k;
   float l;
   float n;
+  float l_n;
 
   l = sqrt(powf(light.x, 2) + powf(light.y, 2) + powf(light.z, 2));
   n = sqrt(powf(normal.x, 2) + powf(normal.y, 2) + powf(normal.z, 2));
   k = (light.x * normal.x) + (light.y * normal.y) + (light.z * normal.z);
-  k = k / (l * n);
+  if ((l_n = l * n) != 0)
+    k = k / l_n;
   if (k >= 0 && k <= 1)
     return (k);
   else if (k > 1)
--- src/rotate.c
+++ src/rotate.c
@@ -5,15 +5,39 @@
 ** Login   <antoine.hartwig@epitech.eu>
 ** 
 ** Started on  Tue Mar  7 16:59:50 2017 HartWoom
-** Last update Tue Mar  7 17:01:40 2017 HartWoom
+** Last update Thu Mar 16 10:14:50 2017 HartWoom
 */
 
 #include "include/my.h"
+
+sfVector3f	rotate_arround_x(sfVector3f vector, float angle)
+{
+  vector.y = vector.y * cos(angle) + vector.z * -sin(angle);
+  vector.z = vector.y * sin(angle) + vector.z * cos(angle);
+  return (vector);
+}
+
+sfVector3f	rotate_arround_y(sfVector3f vector, float angle)
+{
+  vector.x = vector.x * cos(angle) + vector.z * sin(angle);
+  vector.z = vector.x * -sin(angle) + vector.z * cos(angle);
+  return (vector);
+}
+
+sfVector3f	rotate_arround_z(sfVector3f vector, float angle)
+{
+  vector.x = vector.x * cos(angle) + vector.y * -sin(angle);
+  vector.y = vector.x * sin(angle) + vector.y * cos(angle);
+  return (vector);
+}
 
 sfVector3f	rotate_xyz(sfVector3f to_rotate, sfVector3f angles)
 {
   sfVector3f	to_return;
 
+  to_return = rotate_arround_x(to_rotate, angle.x);
+  to_return = rotate_arround_y(to_return, angle.y);
+  to_return = rotate_arround_z(to_return, angle.z);
   return (to_return);
 }
 
@@ -21,5 +45,8 @@ sfVector3f	rotate_zyx(sfVector3f to_rotate, sfVector3f angles)
 {
   sfVector3f	to_return;
 
+  to_return = rotate_arround_z(to_rotate, angle.z);
+  to_return = rotate_arround_y(to_return, angle.y);
+  to_return = rotate_arround_x(to_return, angle.x);
   return (to_return);
 }
